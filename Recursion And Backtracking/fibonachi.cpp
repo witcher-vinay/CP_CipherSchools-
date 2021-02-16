e8f84c2133133225eb42905f054e42dd08759867
witcher-vinay <43574774+witcher-vinay@users.noreply.github.com>
2021-02-16 08:45:12 +0530

    Update fibonachi.cpp

--- Recursion And Backtracking/fibonachi.cpp
+++ Recursion And Backtracking/fibonachi.cpp
@@ -1,1 +1,18 @@
 #include<bits/stdc++.h>
+using namespace std;
+int fibonachi( int n )
+{
+    if(n==1 || n==0)
+    {
+        return 1;
+    }
+    else
+    return fibonachi(n-1) + fibonachi(n-2);
+}
+
+int main()
+{
+    int ans= fibonachi(4);
+    cout << ans << endl ;
+
+}
