bc8f20f902e76317c7bcdb3415c5a91149316778
witcher-vinay <43574774+witcher-vinay@users.noreply.github.com>
2021-02-17 21:45:27 +0530

    Update tailingZeros.cpp

--- Recursion And Backtracking/tailingZeros.cpp
+++ Recursion And Backtracking/tailingZeros.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int trailingZero(int n){
     int count = 0;
-    for (int i = 5; n / i >= 1; i *= 5) count += n / i;
+    for (int i = 5; n / i >= 1; i)
+    {
+        count = count + n / i;
+        i=i*5;
+    } 
     return count;
 }
 int main(){
     int n = 100;
     cout<<trailingZero(n);
-    return 0;
 }
