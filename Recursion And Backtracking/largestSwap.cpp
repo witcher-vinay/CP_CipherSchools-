bedfdd0e2195ed3f23dec4efff21ffe4346daf18
witcher-vinay <43574774+witcher-vinay@users.noreply.github.com>
2021-02-17 18:44:52 +0530

    Create largestSwap.cpp

---
+++ Recursion And Backtracking/largestSwap.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void findMaximumNum(
+    string stringOne, int k,
+    string& max, int count)
+{
+    if (k == 0) return;
+    int n = stringOne.length();
+    char maximumu1 = stringOne[count];
+    for (int j = count + 1; j < n; j++) {
+        if (maximumu1 < stringOne[j]) maximumu1 = stringOne[j];
+    }
+    if (maximumu1 != stringOne[count]) --k;
+ 
+    for (int j = count; j < n; j++) {
+        if (stringOne[j] == maximumu1) {
+            swap(stringOne[count], stringOne[j]);
+            if (stringOne.compare(max) > 0) max = stringOne;
+            findMaximumNum(stringOne, k, max, count + 1);
+            swap(stringOne[count], stringOne[j]);
+        }
+    }
+}
+ 
+int main(){
+    string stringOne = "12989";
+    int k = 4;
+    string max = stringOne;
+    findMaximumNum(stringOne, k, max, 0);
+    cout << max << endl;
+    return 0;
+}
