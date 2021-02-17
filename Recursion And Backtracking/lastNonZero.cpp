ee3f89aa65f1b39b7771b47578550a028f323fed
witcher-vinay <43574774+witcher-vinay@users.noreply.github.com>
2021-02-17 21:44:51 +0530

    Update lastNonZero.cpp

--- Recursion And Backtracking/lastNonZero.cpp
+++ Recursion And Backtracking/lastNonZero.cpp
@@ -1,13 +1,22 @@
 #include<bits/stdc++.h> 
 using namespace std; 
-int dig[] = {1, 1, 2, 6, 4, 2, 2, 4, 2, 8};  
-int nonZero(int n) { 
-    if (n < 10) return dig[n]; 
-    if (((n/10)%10)%2 == 0) return (6*nonZero(n/5)*dig[n%10]) % 10; 
-    else return (4*nonZero(n/5)*dig[n%10]) % 10; 
+int lDid[] = {1, 1, 2, 6};  
+int sol(int n) { 
+    if (n < 10) 
+    {
+        return lDid[n];
+    } 
+    if (((n/10)%10)%2 == 0)
+    {
+      return (6*sol(n/5)*lDid[n%10]) % 10;  
+    }  
+    else
+    {
+        return (4*sol(n/5)*lDid[n%10]) % 10;
+    }  
 }  
 int main() { 
     int n = 14; 
-    cout << nonZero(n); 
+    cout << sol(n); 
     return 0; 
 } 
