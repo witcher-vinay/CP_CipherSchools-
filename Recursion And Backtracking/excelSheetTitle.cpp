a6afa9dacc5d16a8521700ab938b200d5882a1d0
witcher-vinay <43574774+witcher-vinay@users.noreply.github.com>
2021-02-17 21:44:58 +0530

    Update excelSheetTitle.cpp

--- Recursion And Backtracking/excelSheetTitle.cpp
+++ Recursion And Backtracking/excelSheetTitle.cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-string convertToTitle(int n) {
+string convert(int N) {
     string res;
-    while (n>0) {
-        n--;
+    while (N>0) 
+    {
+        N--;
         res = char('A' + n%26) + res;
-        n /= 26; 
+        N = N/26; 
     }
     return res;
 }
-
 int main(){
     int n = 28;
-    cout<<convertToTitle(n);
+    cout<<convert(n);
     return 0;
 }
