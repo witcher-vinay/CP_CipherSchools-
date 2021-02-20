35bfaf22f652b284c9d086c74586b58156ff9ad3
witcher-vinay <43574774+witcher-vinay@users.noreply.github.com>
2021-02-20 21:09:27 +0530

    Create isTreeMirror.cpp

---
+++ trees/isTreeMirror.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode {
+    public:
+    int data;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(int data) {
+        this->data = data;
+        this->left = this->right = NULL;
+    }
+    TreeNode() {
+        TreeNode(0);
+    }
+};
+
+bool isMirror(TreeNode * a, TreeNode * b) {
+    if (a == NULL && b == NULL) return true;
+    if (a == NULL || b == NULL) return false;
+
+    if (a->data == b->data) {
+        return isMirror(a->left, b->right) && isMirror(a->right, b->left);
+    } else {
+        return false;
+    }
+}
+
+bool isMiror(TreeNode * root) {
+    return isMirror(root->left, root->right);
+}
