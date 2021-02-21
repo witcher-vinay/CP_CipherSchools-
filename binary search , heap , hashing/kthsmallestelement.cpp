2ad176451700f2e73bef791c59002a310e79081a
witcher-vinay <43574774+witcher-vinay@users.noreply.github.com>
2021-02-21 20:46:06 +0530

    Update kthsmallestelement.cpp

--- binary search , heap , hashing/kthsmallestelement.cpp
+++ binary search , heap , hashing/kthsmallestelement.cpp
@@ -1,12 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
 int kthSmallestElement(vector<int>& vec, int k) {
-    // priority_queue<int, vector<int>, greater<int>> gq) PQ => Min Heap
     priority_queue<int> PQ; 
     for (int i = 0; i < vec.size(); i++) {
-        // 0..k-1
         PQ.push(vec[i]);
 
         if (PQ.size() > k) {
@@ -15,7 +11,6 @@ int kthSmallestElement(vector<int>& vec, int k) {
     }
     return PQ.top();
 }
-
 int main() {
     vector<int> vec = {7, 10, 4, 3, 19, 2, 14, 16, 4};
     for (int i = 0; i < vec.size(); i++) {
