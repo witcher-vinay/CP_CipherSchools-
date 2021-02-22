864c6c53dfdbea48efdd26bec0adc94b3cd742f3
witcher-vinay <43574774+witcher-vinay@users.noreply.github.com>
2021-02-22 21:22:21 +0530

    Create uniquePaths.cpp

---
+++ Dynamic programming/uniquePaths.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std; 
+int numberOfPaths(int m, int n) 
+{ t 
+    if (m == 1 || n == 1) 
+        return 1;  
+    return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1); 
+} 
+  
+int main() 
+{ 
+    cout << numberOfPaths(3, 3); 
+    return 0; 
+} 
