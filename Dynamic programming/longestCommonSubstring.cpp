d4e3571a795296ff8fad0843e93caa58437f3741
witcher-vinay <43574774+witcher-vinay@users.noreply.github.com>
2021-02-22 21:22:33 +0530

    Create longestCommonSubstring.cpp

---
+++ Dynamic programming/longestCommonSubstring.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std; 
+string X, Y;
+int lcs(int i, int j, int count)
+{
+ 
+    if (i == 0 || j == 0)
+        return count;
+ 
+    if (X[i - 1] == Y[j - 1]) {
+        count = lcs(i - 1, j - 1, count + 1);
+    }
+    count = max(count,
+                max(lcs(i, j - 1, 0),
+                    lcs(i - 1, j, 0)));
+    return count;
+}
+int main()
+{
+    int n, m;
+ 
+    X = "abcdxyz";
+    Y = "xyzabcd";
+ 
+    n = X.size();
+    m = Y.size();
+ 
+    cout << lcs(n, m, 0);
+ 
+    return 0;
+}
